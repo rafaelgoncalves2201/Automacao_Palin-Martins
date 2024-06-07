6234b6965e962f5487920d1dd60f9efd6fc2f828
Rafael Gonçalves <156006438+rafaelgoncalves2201@users.noreply.github.com>
2024-06-07 17:51:45 -0300

    Update Teste_oficial.py

--- Coleta de dados Aiim/Teste_oficial.py
+++ Coleta de dados Aiim/Teste_oficial.py
@@ -52,57 +52,61 @@ try:
         # Espere até que o botão de pesquisa seja clicável e clique nele
         pesquisar = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@title='Clique para pesquisar por numero do aiim (sem o digito verificador)']")))
         pesquisar.click()
-        
 
-        ERRO = "" # String Vazia
-        DRT = ""
-        
         try:
-            # Espera até que o elemento com o ID 'ConteudoPagina_lblDRT' seja visível e obtenha o texto
-            elemento_drt = wait.until(EC.visibility_of_element_located((By.ID, 'ConteudoPagina_lblDRT')))
-            DRT = elemento_drt.text
-
-            # Pesquisar nome
-            elemento_nome = driver.find_element(By.ID, 'ConteudoPagina_lblNomeAutuado')
-            NOME = elemento_nome.text
-
-            # Pesquisar data
-            elemento_data = driver.find_element(By.CSS_SELECTOR, 'td.td1#dataEvento')
-            DATA = elemento_data.text
-            
-            
-        except Exception as e:
-            # Se ocorrer uma exceção durante a busca de informações, registre o erro na planilha
-            ERRO = str(e)
-
-        finally:
-            # Exibe a data do Dia
-            sheet.cell(row=linha_planilha, column=9).value = DATE
-            # Verificar se há erro e escrever na planilha
-            if ERRO == "":
-                sheet.cell(row=linha_planilha, column=2).value = DRT
-                sheet.cell(row=linha_planilha, column=3).value = DATA
-                sheet.cell(row=linha_planilha, column=4).value = NOME
-                sheet.cell(row=linha_planilha, column=10).value = "Passar Click Up"
-                for col in range(1, 11):
-                    sheet.cell(row=linha_planilha, column=col).fill = cor_clickup
-            else:
-                sheet.cell(row=linha_planilha, column=2).value = "Erro"
-                sheet.cell(row=linha_planilha, column=10).value = "Não tem ainda"
-                for col in range(1, 11):
-                    sheet.cell(row=linha_planilha, column=col).fill = cor_naotem
-                
-            # Verificar se DRT está na lista outros
-            if DRT in outros:
-                sheet.cell(row=linha_planilha, column=10).value = "Outros"
-                for col in range(1, 11):
-                    sheet.cell(row=linha_planilha, column=col).fill = cor_outros
-
-            # Incrementar o contador de linha em qualquer caso
-            linha_planilha += 1
-
-            # Volte para a página inicial após cada iteração
+            alert_wait = WebDriverWait(driver, timeout=0.5)
+            alert = alert_wait.until(EC.alert_is_present())
             driver.get('https://www.fazenda.sp.gov.br/epat/extratoprocesso/PesquisarExtrato.aspx')
+        except Exception as e:    
+                ERRO = "" # String Vazia
+                DRT = ""
+                
+                try:
+                    # Espera até que o elemento com o ID 'ConteudoPagina_lblDRT' seja visível e obtenha o texto
+                    elemento_drt = wait.until(EC.visibility_of_element_located((By.ID, 'ConteudoPagina_lblDRT')))
+                    DRT = elemento_drt.text
+        
+                    # Pesquisar nome
+                    elemento_nome = driver.find_element(By.ID, 'ConteudoPagina_lblNomeAutuado')
+                    NOME = elemento_nome.text
+        
+                    # Pesquisar data
+                    elemento_data = driver.find_element(By.CSS_SELECTOR, 'td.td1#dataEvento')
+                    DATA = elemento_data.text
+                    
+                    
+                except Exception as e:
+                    # Se ocorrer uma exceção durante a busca de informações, registre o erro na planilha
+                    ERRO = str(e)
+        
+                finally:
+                    # Exibe a data do Dia
+                    sheet.cell(row=linha_planilha, column=9).value = DATE
+                    # Verificar se há erro e escrever na planilha
+                    if ERRO == "":
+                        sheet.cell(row=linha_planilha, column=2).value = DRT
+                        sheet.cell(row=linha_planilha, column=3).value = DATA
+                        sheet.cell(row=linha_planilha, column=4).value = NOME
+                        sheet.cell(row=linha_planilha, column=10).value = "Passar Click Up"
+                        for col in range(1, 11):
+                            sheet.cell(row=linha_planilha, column=col).fill = cor_clickup
+                    else:
+                        sheet.cell(row=linha_planilha, column=2).value = "Erro"
+                        sheet.cell(row=linha_planilha, column=10).value = "Não tem ainda"
+                        for col in range(1, 11):
+                            sheet.cell(row=linha_planilha, column=col).fill = cor_naotem
+                        
+                    # Verificar se DRT está na lista outros
+                    if DRT in outros:
+                        sheet.cell(row=linha_planilha, column=10).value = "Outros"
+                        for col in range(1, 11):
+                            sheet.cell(row=linha_planilha, column=col).fill = cor_outros
+        
+                    # Incrementar o contador de linha em qualquer caso
+                    linha_planilha += 1
+        
+                    # Volte para a página inicial após cada iteração
+                    driver.get('https://www.fazenda.sp.gov.br/epat/extratoprocesso/PesquisarExtrato.aspx')
 
 finally:
     # Salvar o arquivo Excel
